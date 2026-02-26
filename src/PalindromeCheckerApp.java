f84589561c0620c0d9a73a922ac717147c5022fe
Smitiron05 <smitironbehera11@gmail.com>
2026-02-26 10:31:10 +0530

    UC4:Hardcoded palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,21 +1,19 @@
-import java.util.Scanner;
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Input text : ");
-        String input = scanner.nextLine();
-        String reversed = "";
-        for (int i = input.length() - 1; i >= 0; i--)
-        {
-            reversed = reversed + input.charAt(i);
+        String input = "radar";
+        char[] chars = input.toCharArray();
+        int start = 0;
+        int end = chars.length - 1;
+        boolean isPalindrome = true;
+        while (start < end) {
+            if (chars[start] != chars[end]) {
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
         }
-        if (input.equals(reversed)) {
-            System.out.println("Is it a Palindrome? Yes");
-        }
-        else
-        {
-            System.out.println("Is it a Palindrome? No");
-        }
-        scanner.close();
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome? : " + isPalindrome);
     }
 }
